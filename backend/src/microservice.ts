c827bfe23603703ab0f718139a32899cee3586ee
Modall dartora da silva <dartorandroid@gmail.com>
2025-03-08 13:32:06 -0300

    adding microservice nestjs

---
+++ backend/src/microservice.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+
+async function bootstrapMicroservice() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://rabbitmq:5672'], // URL do RabbitMQ
+        queue: 'consent_events', // Nome da fila
+        queueOptions: {
+          durable: false, // Define se a fila é durável
+        },
+      },
+    },
+  );
+
+  await app.listen();
+}
+
+bootstrapMicroservice();
